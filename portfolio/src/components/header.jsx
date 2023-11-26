822c7995d2284674a43ff876fed9540dace30373
Armagdo <52860974+MostafaSaad1987@users.noreply.github.com>
2023-11-26 07:36:29 +0200

    CSS Changes

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -5,6 +5,9 @@ const Header = () => {
         <header>
             <h1>Hello. I'm Mostafa Hassabelnaby.</h1>
             <h2>*Job Title Here* That *something good here* and *another positive here*</h2>
+            <div className="move">
+                <a href=".skills"> Placeholder </a>
+            </div>
         </header>
     );
 };
