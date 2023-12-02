77897aa20faf1aff0013c63b8898b725e9f0835c
Armagdo <52860974+MostafaSaad1987@users.noreply.github.com>
2023-12-02 21:22:12 +0200

    Almost Done
    Waiting for the phone screenshots.

--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const githubLink = "https://github.com/MostafaSaad1987/";
 const liveGithubLink = "https://mostafasaad1987.github.io/";
@@ -14,23 +14,61 @@ const Projects = [
     },
     {
         text: "Tic Tac Toe made with HTML, CSS, JavaScript.",
-        skills: "",
+        skills: "HTML, CSS, JS",
         "project-name": "Tic Tac Toe",
-        "project-source": "odin-tic-tac-toe/",
-        "project-preview": "../src/assets/screenshots/TikTacToe.png",
+        "project-live": "odin-tic-tac-toe/",
+        "project-preview": "../src/assets/screenshots/TicTacToe.png",
+        "project-preview-mobile": "",
+    },
+    {
+        text: "A website to track all the shows that you watch. (Or at least the ones that are available TVMaze.)",
+        skills: "HTML, CSS, JS, Axios",
+        "project-name": "TV Shows Library/Tracker",
+        "project-live": "TV-Show-Library/",
+        "project-preview": "../src/assets/screenshots/TVTraker.png",
         "project-preview-mobile": "",
     },
 ];
 
 const Project = React.forwardRef((props, ref) => {
-    const [currentProject, setProjectState] = useState(Projects[0]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentProject, setProjectState] = useState(Projects[currentIndex]);
+    const [projectClass, setProjectClass] = useState("");
+
+    useEffect(() => {
+        console.log("Current project: ", currentProject);
+        console.log("Current index: ", currentIndex);
+    }, [currentIndex]);
+
+    const handleClick = (event) => {
+        setProjectClass("hidden");
+
+        setTimeout(() => {
+            const newIndex = (currentIndex + 1) % Projects.length;
+            setCurrentIndex(newIndex);
+            setProjectState(Projects[newIndex]);
+            setProjectClass("");
+        }, 250);
+    };
+
+    const handleBackClick = (event) => {
+        setProjectClass("hidden");
+
+        setTimeout(() => {
+            const newIndex = (currentIndex - 1 + Projects.length) % Projects.length;
+            setCurrentIndex(newIndex);
+            setProjectState(Projects[newIndex]);
+            setProjectClass("");
+        }, 250);
+    };
+
     return (
         <div className="projects" ref={ref}>
             <h1>Projects</h1>
-            <div className="project">
-                {currentProject["project-name"]}
+            <div className={`project ${projectClass}`}>
                 <div className="project-links">
                     <a href={githubLink + currentProject["project-live"]}>Source Code</a>
+                    <h2>{currentProject["project-name"]}</h2>
                     <a href={liveGithubLink + currentProject["project-live"]}>Live Preview</a>
                 </div>
                 <p className="project-text">{currentProject.text}</p>
@@ -39,11 +77,12 @@ const Project = React.forwardRef((props, ref) => {
                 <img className="project-mobile-image" src={currentProject["project-preview-mobile"]} alt="Project Preview" />
             </div>
             <div className="projects-buttons">
-                <button>Previous Project</button>
-                <button>Next Project</button>
+                <button onClick={handleBackClick}>Previous Project</button>
+                <button onClick={handleClick}>Next Project</button>
             </div>
         </div>
     );
 });
+
 
 export default Project;
