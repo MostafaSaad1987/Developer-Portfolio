349899a6cfe4c05dadf640783238a82a1167d44b
Mostafa Hassabelnaby <52860974+MostafaSaad1987@users.noreply.github.com>
2024-10-21 22:49:22 +0300

    Removed Silly Part.

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -5,7 +5,6 @@ const Header = React.forwardRef((props, ref) => {
         <header ref={ref}>
             <h1>Hello. I'm Mostafa Hassabelnaby.</h1>
             <h2>Front-end Developer</h2>
-            <h6>Fullstack under training.</h6>
         </header>
     );
 });
